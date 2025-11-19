c474d7e454c28383bf1ec38096c804ff70af4526
whatisRT <andre.knispel@iohk.io>
2025-11-19 16:43:03 +0100

    Use `agda.nix` & some Agda 2.8 compatibility

--- nix/outputs.nix
+++
@@ -1,9 +0,0 @@
-{ repoRoot, inputs, pkgs, lib, system }:
-
-let
-  agda = import ./agda.nix {inherit pkgs lib inputs;};
-in
-[{
-    devShells.default = repoRoot.nix.shell;
-    packages = agda;
-}]
