9db6c8ef0d63938013d2a5a97664ef37215b1a90
Yves Hauser <yves.hauser@iohk.io>
2026-09-21 14:10:00 +0200

    Updated reference to categorical-crypto

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     agda-nix = {
       url = "github:input-output-hk/agda.nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.categorical-crypto.url = "github:input-output-hk/categorical-crypto/893586d45cb91827800a08b952d0bb6503659769";
+      inputs.categorical-crypto.url = "github:input-output-hk/categorical-crypto/56497e3ff080c18cfe0bbde4b1d35b3d307c9001";
     };
   };
 
